2f32658f23564404189b3e1a04bc2aa658296829
harshit singhal <59227793+0-harshit-0@users.noreply.github.com>
2020-09-05 10:57:18 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -118,115 +118,116 @@ class Vector2D {
 
 
 class Shapes {
-	constructor() {
+	constructor(context) {
+		this.c = context;
 		//console.log('working');
 
 		//c/tx.lineCap = 'round';
 	}
-	static strokeLine(a, b, c, d, colour) {
-		ctx.beginPath();
-		ctx.strokeStyle = colour;
-		ctx.moveTo(a, b);
-		ctx.lineTo(c, d);
-		ctx.stroke();
-		ctx.closePath();
+	strokeLine(a, b, c, d, colour) {
+		this.c.beginPath();
+		this.c.strokeStyle = colour;
+		this.c.moveTo(a, b);
+		this.c.lineTo(c, d);
+		this.c.stroke();
+		this.c.closePath();
 	}
-	static fillLine(a, b, c, d, colour) {
-		ctx.beginPath();
-		ctx.strokeStyle = colour;
-		ctx.moveTo(a, b);
-		ctx.lineTo(c, d);
-		ctx.fill();
-		ctx.closePath();
+	fillLine(a, b, c, d, colour) {
+		this.c.beginPath();
+		this.c.fillStyle = colour;
+		this.c.moveTo(a, b);
+		this.c.lineTo(c, d);
+		this.c.fill();
+		this.c.closePath();
 	}
-	static fillBox(x, y, w, h, colour) {
-		ctx.beginPath();
-		ctx.fillStyle = colour;
-		ctx.fillRect(x, y, w, h);
-		ctx.closePath();
+	fillBox(x, y, w, h, colour) {
+		this.c.beginPath();
+		this.c.fillStyle = colour;
+		this.c.fillRect(x, y, w, h);
+		this.c.closePath();
 	}
-	static strokeBox(x, y, w, h, colour) {
-		ctx.beginPath();
-		ctx.strokeStyle = colour;
-		ctx.strokeRect(x, y, w, h);
-		ctx.closePath();
+	strokeBox(x, y, w, h, colour) {
+		this.c.beginPath();
+		this.c.strokeStyle = colour;
+		this.c.strokeRect(x, y, w, h);
+		this.c.closePath();
 	}
-	static strokeCir(x, y, r, colour) {
-		ctx.beginPath();
-		ctx.strokeStyle = colour;
-		ctx.arc(x, y, r, 0, Math.PI*2, false);
-		ctx.stroke();
-		ctx.closePath();
+	strokeCir(x, y, r, colour) {
+		this.c.beginPath();
+		this.c.strokeStyle = colour;
+		this.c.arc(x, y, r, 0, Math.PI*2, false);
+		this.c.stroke();
+		this.c.closePath();
 	}
-	static fillCir(x, y, r, colour) {
-		ctx.beginPath();
-		ctx.strokeStyle = colour;
-		ctx.arc(x, y, r, 0, Math.PI*2, false);
-		ctx.fill();
-		ctx.closePath();
+	fillCir(x, y, r, colour) {
+		this.c.beginPath();
+		this.c.strokeStyle = colour;
+		this.c.arc(x, y, r, 0, Math.PI*2, false);
+		this.c.fill();
+		this.c.closePath();
 	}
 
 	//storke and fill
-	static line(a, b, c, d, colourS, colourF) {
-		ctx.beginPath();
-		ctx.strokeStyle = colourS;
-		ctx.fillStyle = colourF;
-		ctx.moveTo(a, b);
-		ctx.lineTo(c, d);
-		ctx.stroke();
-		ctx.fill();
-		ctx.closePath();
+	line(a, b, c, d, colourS, colourF) {
+		this.c.beginPath();
+		this.c.strokeStyle = colourS;
+		this.c.fillStyle = colourF;
+		this.c.moveTo(a, b);
+		this.c.lineTo(c, d);
+		this.c.stroke();
+		this.c.fill();
+		this.c.closePath();
 	}
 	
-	static box(x, y, w, h, colourS, colourF) {
-		ctx.beginPath();
-		ctx.strokeStyle = colourS;
-		ctx.fillStyle = colourF;
-		ctx.fillRect(x, y, w, h);
-		ctx.strokeRect(x, y, w, h);
-		ctx.closePath();
+	box(x, y, w, h, colourS, colourF) {
+		this.c.beginPath();
+		this.c.strokeStyle = colourS;
+		this.c.fillStyle = colourF;
+		this.c.fillRect(x, y, w, h);
+		this.c.strokeRect(x, y, w, h);
+		this.c.closePath();
 	}
-	static circle(x, y, r, colourS, colourF) {
-		ctx.beginPath();
-		ctx.strokeStyle = colourS;
-		ctx.fillStyle = colourF;
-		ctx.arc(x, y, r, 0, Math.PI*2, false);
-		ctx.stroke();
-		ctx.fill();
-		ctx.closePath();
+	circle(x, y, r, colourS, colourF) {
+		this.c.beginPath();
+		this.c.strokeStyle = colourS;
+		this.c.fillStyle = colourF;
+		this.c.arc(x, y, r, 0, Math.PI*2, false);
+		this.c.stroke();
+		this.c.fill();
+		this.c.closePath();
 	}
 
 	//still working
-	static fillEqTri(l, colour) {
-		ctx.beginPath();
-		ctx.fillStyle = colour;
-		ctx.moveTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*120), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*120));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*240), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*240));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
-		ctx.fill();
-		ctx.closePath();
+	fillEqTri(l, colour) {
+		this.c.beginPath();
+		this.c.fillStyle = colour;
+		this.c.moveTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*120), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*120));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*240), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*240));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
+		this.c.fill();
+		this.c.closePath();
 	}
-	static strokeEqTri(l, colour) {
-		ctx.beginPath();
-		ctx.strokeStyle = colour;
-		ctx.moveTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*120), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*120));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*240), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*240));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
-		ctx.stroke();
-		ctx.closePath();
+	strokeEqTri(l, colour) {
+		this.c.beginPath();
+		this.c.strokeStyle = colour;
+		this.c.moveTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*120), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*120));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*240), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*240));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
+		this.c.stroke();
+		this.c.closePath();
 	} 
-	static EqTri(l, colourS, colourF) {
-		ctx.beginPath();
-		ctx.strokeStyle = colourS;
-		ctx.fillStyle = colourF;
-		ctx.moveTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*120), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*120));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*240), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*240));
-		ctx.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
-		ctx.stroke();
-		ctx.fill();
-		ctx.closePath();
+	EqTri(l, colourS, colourF) {
+		this.c.beginPath();
+		this.c.strokeStyle = colourS;
+		this.c.fillStyle = colourF;
+		this.c.moveTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*120), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*120));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*240), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*240));
+		this.c.lineTo(l*(Math.sqrt(3)/3) * Math.cos(Math.PI/180*0), l*(Math.sqrt(3)/3) * Math.sin(Math.PI/180*0));
+		this.c.stroke();
+		this.c.fill();
+		this.c.closePath();
 	} 
 }
