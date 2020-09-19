667ee3319b22dd176ed4526d1fd14eeec67b4088
harshit singhal <59227793+0-harshit-0@users.noreply.github.com>
2020-09-19 16:18:04 +0530

    Update shpes.js

--- Assets/shpes.js
+++ Assets/shpes.js
@@ -1,53 +1,7 @@
 class Shapes {
 	constructor(context) {
 		this.c = context;
-		//console.log('working');
-
-		//c/tx.lineCap = 'round';
 	}
-	strokeLine(a, b, c, d, colour) {
-		this.c.beginPath();
-		this.c.strokeStyle = colour;
-		this.c.moveTo(a, b);
-		this.c.lineTo(c, d);
-		this.c.stroke();
-		this.c.closePath();
-	}
-	fillLine(a, b, c, d, colour) {
-		this.c.beginPath();
-		this.c.fillStyle = colour;
-		this.c.moveTo(a, b);
-		this.c.lineTo(c, d);
-		this.c.fill();
-		this.c.closePath();
-	}
-	fillBox(x, y, w, h, colour) {
-		this.c.beginPath();
-		this.c.fillStyle = colour;
-		this.c.fillRect(x, y, w, h);
-		this.c.closePath();
-	}
-	strokeBox(x, y, w, h, colour) {
-		this.c.beginPath();
-		this.c.strokeStyle = colour;
-		this.c.strokeRect(x, y, w, h);
-		this.c.closePath();
-	}
-	strokeCir(x, y, r, colour) {
-		this.c.beginPath();
-		this.c.strokeStyle = colour;
-		this.c.arc(x, y, r, 0, Math.PI*2, false);
-		this.c.stroke();
-		this.c.closePath();
-	}
-	fillCir(x, y, r, colour) {
-		this.c.beginPath();
-		this.c.strokeStyle = colour;
-		this.c.arc(x, y, r, 0, Math.PI*2, false);
-		this.c.fill();
-		this.c.closePath();
-	}
-
 	//storke and fill
 	line(a, b, c, d, colourS, colourF) {
 		this.c.beginPath();
@@ -55,17 +9,14 @@ class Shapes {
 		this.c.fillStyle = colourF;
 		this.c.moveTo(a, b);
 		this.c.lineTo(c, d);
-		this.c.stroke();
-		this.c.fill();
 		this.c.closePath();
 	}
 	
-	box(x, y, w, h, colourS, colourF) {
+	box(x, y, w, h, colourS='black', colourF='black') {
 		this.c.beginPath();
 		this.c.strokeStyle = colourS;
 		this.c.fillStyle = colourF;
-		this.c.fillRect(x, y, w, h);
-		this.c.strokeRect(x, y, w, h);
+		this.c.rect(x, y, w, h);
 		this.c.closePath();
 	}
 	circle(x, y, r, colourS, colourF) {
@@ -73,8 +24,6 @@ class Shapes {
 		this.c.strokeStyle = colourS;
 		this.c.fillStyle = colourF;
 		this.c.arc(x, y, r, 0, Math.PI*2, false);
-		this.c.stroke();
-		this.c.fill();
 		this.c.closePath();
 	}
 
