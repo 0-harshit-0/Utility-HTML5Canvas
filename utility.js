419c55a98ee63d5f05561005cb600a31bad801e5
harshit singhal <59227793+0-harshit-0@users.noreply.github.com>
2020-11-28 14:28:51 +0530

    Add files via upload

--- utility.js
+++ utility.js
@@ -1,22 +1,21 @@
 class Stack {
 	constructor() {
-		this.index = 0;
+		this.size = 0;
 		this.stackarray = new Array();
 	}
 	push(value) {
 
 		this.stackarray.unshift(value);
-		this.index++;
+		this.size++;
 
 		return value;
 	}
 	pop() {
-		
-		if (this.index < 0) {
-			this.index = 0;
+		if (this.size < 0) {
+			this.size = 0;
 			throw 'Stack is empty';
 		}
-		this.index--;
+		this.size--;
 		return this.stackarray.shift();
 		//return this.stackarray;
 	}
@@ -24,7 +23,6 @@ class Stack {
 		return this.stackarray;
 	}
 }
-
 
 class Queues {
 	constructor() {
@@ -56,7 +54,6 @@ class Node {
 		this.next = null;
 	}
 }
-
 class LinkedList {
 	constructor() {
 		this.head = null;
@@ -167,7 +164,7 @@ class LinkedList {
 
 
 class Graph {
-	constructor(vertices=10) {
+	constructor(vertices) {
 		this.v = vertices;
 		this.list = new Array();
 
@@ -188,7 +185,7 @@ class Graph {
 	}
 	//travers
 	dfs(root) {
-		//console.log(root);
+		console.log(root);
 		let l = this.list;
 		l[root].visited = true;
 		let curr = l[root];
@@ -346,6 +343,7 @@ class Vector2D {
 }
 //combine vector and shapes
 //stroke width
+
 class Shapes {
 	constructor(context) {
 		this.c = context;
@@ -353,27 +351,27 @@ class Shapes {
 
 		//c/tx.lineCap = 'round';
 	}
-	fill(clr = 'black') {
+	fill(clr) {
 		this.c.fillStyle = clr;
 		this.c.fill();
 	}
-	stroke(clr = 'black') {
+	stroke(clr) {
 		this.c.strokeStyle = clr;
 		this.c.stroke();
 	}
 	//storke and fill
-	line(a, b, c, d) {
+	line(a=0, b=0, c=10, d=10) {
 		this.c.beginPath();
 		this.c.moveTo(a, b);
 		this.c.lineTo(c, d);
 		//this.c.closePath();
 	}
-	box(x, y, w, h) {
+	box(x=0, y=0, w=10, h=10) {
 		this.c.beginPath();
 		this.c.rect(x, y, w, h);
 		//this.c.closePath();
 	}
-	circle(x, y, r) {
+	circle(x=0, y=0, r=10) {
 		this.c.beginPath();
 		this.c.arc(x, y, r, 0, Math.PI*2, false);
 		//this.c.closePath();
@@ -418,4 +416,61 @@ class Shapes {
 		this.c.closePath();
 		this.c.restore();
 	}
+}
+
+
+//noises=================================================
+class Gradient{
+	constructor() {
+		this.vector2 = new Vector2D();
+	}
+	
+	randomGradient(a, b) {
+		let random = 2920 * Math.sin(a * 21942.0 + b * 171324.0 + 8912.0) * Math.cos(a * 23157.0 + b * 217832.0 + 9758.0);
+		return this.vector2 = new Vector2D(Math.cos(random), Math.sin(random));
+	}
+	dotGridGradient(ia, ib, a, b) {
+		let gradient = this.randomGradient(ia, ib);
+
+		// Compute the distance vector
+		let dx = a - ia;
+		let dy = b - ib;
+		
+		// Compute the dot-product
+		return (dx*gradient.x + dy*gradient.y);
+	}
+	static fade(t) {
+		return t * t * t * (t * (t * 6 - 15) + 10);
+	}
+	static lerp(a0, a1, w) {
+		return Gradient.fade(1.0 - w)*a0 + Gradient.fade(w)*a1;
+	}
+}
+
+function perlin(a, b) {
+	let gr = new Gradient();
+    // Determine grid cell coordinates
+    let x0 = Math.floor(a);
+    let x1 = x0 + 1;
+    let y0 = Math.floor(b);
+    let y1 = y0 + 1;
+
+    // Determine interpolation weights
+    // Could also use higher order polynomial/s-curve here
+    let sx = a - x0;
+    let sy = b - y0;
+
+    // Interpolate between grid point gradients
+    let n0, n1, ia0, ia1, value;
+
+    n0 = gr.dotGridGradient(x0, y0, a, b);
+    n1 = gr.dotGridGradient(x1, y0, a, b);
+    ia0 = Gradient.lerp(n0, n1, sx);
+
+    n0 = gr.dotGridGradient(x0, y1, a, b);
+    n1 = gr.dotGridGradient(x1, y1, a, b);
+    ia1 = Gradient.lerp(n0, n1, sx);
+
+    value = Gradient.lerp(ia0, ia1, sy);
+    return value;
 }
