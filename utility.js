b090431f62d64b8158dac95b26f9733c80e35d16
harshit singhal <harshitw3b@gmail.com>
2022-01-08 19:47:54 +0530

    garph

--- utility.js
+++ utility.js
@@ -162,64 +162,98 @@ class LinkedList {
 	}
 }
 
-
-class Graph {
+class GNode {
+	constructor(data) {
+		this.d = new Set().add(data);
+		this.visited = false;
+	}
+}
+class UGraph {
 	constructor(vertices) {
 		this.v = vertices;
-		this.list = new Array();
-
-		for (var i = 0; i < this.v; i++) {
-			this.list.push(new LinkedList());
+		this.AdjList = new Map();
+		this.length = 0;
+	}
+	add(v, dv) { //sv | source vertices
+		if(v == dv) return 0;
+		if(this.AdjList.has(v)) {
+			this.AdjList.get(v).d.add(dv);
+		}else{
+			if(this.length >= this.v) return 0;
+			this.AdjList.set(v, new GNode(dv));
+			this.length++;
+		}
+		if(this.AdjList.has(dv)) {
+			this.AdjList.get(dv).d.add(v);
+		}else{
+			if(this.length >= this.v) return 0;
+			this.AdjList.set(dv, new GNode(v));
+			this.length++;
 		}
 	}
-	addEdge(source, des) {
-		if (source >= 0 && source <= this.v && source !== des) {
-			this.list[source].add(des);
+	adjacent(v1, v2) {
+		return this.AdjList.get(v1).d.has(v2);
+	}
+	neighbors(v1) {
+		return this.AdjList.get(v1).d;
+	}
+	remove(v, dv=false) {
+		if(!dv) {
+			this.AdjList.delete(v);
+		}else{
+			this.AdjList.get(v).d.delete(dv);
 		}
 	}
 	view() {
-		for (var i = 0; i < this.list.length; i++) {
-			console.log("list: "+i);
-			this.list[i].view();
+		const iterator1 = this.AdjList[Symbol.iterator]();
+
+		for (const item of iterator1) {
+			console.log(item);
+			console.log(item[1].d);
 		}
 	}
 	//travers
 	dfs(root) {
-		console.log(root);
-		let l = this.list;
-		l[root].visited = true;
-		let curr = l[root];
-		let connections = l[root].size;
-
-		if (connections) {
-			for (var i = 0; i < connections; i++) {
-				let ele = l[root].remove(0);
-				if (!l[ele].visited) {
-					this.dfs(ele);
-					//visited = ele;
-				}
+		console.log(root)
+		//console.log(this.AdjList.get(root))
+		this.AdjList.get(root).visited = true;
+		[...this.neighbors(root)].forEach(z=> {
+			if(!this.AdjList.get(z).visited){
+				this.dfs(z);
+			}
+		});
+	}
+	dfsS(root) {
+		let s = new Stack();
+		s.push(root);
+		while(s.length) {
+			root = s.pop();
+			if(!this.AdjList.get(root).visited) {
+				console.log(root)
+				this.AdjList.get(root).visited = true;
+				[...this.neighbors(root)].forEach(z=> {
+					s.push(z);
+				});
 			}
 		}
 	}
 
 	bfs(root) {
 		let q = new Queues();
-		let l = this.list;
-		l[root].visited = true;
+		this.AdjList.get(root).visited = true;
 		q.push(root);
 
-		while(q.size) {
+		while(q.length) {
 			let v = q.pop();
 			console.log(v);
-			//if (v ) {}
-			let connections = l[v].size;
-			for (var i = 0; i < connections; i++) {
-				let ele = l[v].remove(0);
-				if (!l[ele].visited) {
-					l[ele].visited = true;
-					q.push(ele);
+			//if (v == root) {}
+			[...this.neighbors(v)].forEach(z=> {
+				if(!this.AdjList.get(z).visited){
+					
+					this.AdjList.get(z).visited = true;
+					q.push(z);
 				}
-			}
+			});
 		}
 	}
 }
